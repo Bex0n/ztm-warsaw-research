757179895ec7abef542c57ea31bd8c48b0cedeeb
Jakub Dawidowicz <j.dawidowicz@omnitrace.io>
2024-01-11 14:34:37 +0100

    Add api key for bus caller

--- src/ztmwarsaw/api/bus_caller.py
+++ src/ztmwarsaw/api/bus_caller.py
@@ -1,14 +1,20 @@
+import json
+
 from .api_caller import APICaller
-import urllib
+from urllib.request import urlopen
 
 
 class BusCaller(APICaller):
     def __init__(self):
         APICaller.__init__(self)
 
-    def build(self, url='https://api.um.warszawa.pl/api/action/datastore_search'):
+    def build(self,
+              url='https://api.um.warszawa.pl/api/action/datastore_search',
+              api_key=None):
         self.url = url
+        self.api_key = api_key
 
-    def call(self, **kwargs):
-        fileobj = urllib.urlopen(self.url)
-        return fileobj.read()
+    def call(self, *args):
+        with urlopen(self.url) as url:
+            response = url.read()
+        return json.loads(response.decode("utf-8"))
