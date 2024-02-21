9ad6582485aaad23d68042b81457b17ac85a4445
Jakub Dawidowicz <jdawidowicz@poczta.onet.pl>
2024-02-21 18:37:49 +0100

    More visualisations

--- src/ztmwarsaw/api/BusCaller.py
+++ src/ztmwarsaw/api/BusCaller.py
@@ -10,24 +10,31 @@ class BusCaller(ICaller):
         ICaller.__init__(self)
         self.location_url = "https://api.um.warszawa.pl/api/action/busestrams_get/"
         self.schedule_url = "https://api.um.warszawa.pl/api/action/dbtimetable_get/"
-        self.resource_id = "f2e5503e-927d-4ad3-9500-4ab9e55deb59"
+        self.stop_url = "https://api.um.warszawa.pl/api/action/dbstore_get/"
+        self.location_resource_id = "f2e5503e-927d-4ad3-9500-4ab9e55deb59"
+        self.stop_resource_id = "ab75c33d-3a26-4342-b36a-6e5fef0a3ac3"
         self.apikey = apikey
         self.vehicle_type = 1
 
-    def __get_obligatory_params(self, params: LocationRequest) -> Dict[str, Any]:
+    def __get_location_obligatory_params(self, params: Dict[str, Any]) -> Dict[str, Any]:
         obligatory_params = {
             "apikey": self.apikey,
-            "resource_id": self.resource_id,
+            "resource_id": self.location_resource_id,
             "type": self.vehicle_type,
-            **params.dict(),
+            **params,
         }
         return obligatory_params
 
-    def __get_data(self, url: str, params: LocationRequest) -> Optional[Dict]:
-        params_dict = self.__get_obligatory_params(params)
-        print("Url: ", url)
-        print("Params: ", params_dict)
-        response = requests.get(url, params=params_dict)
+    def __get_stop_obligatory_params(self, params: Dict[str, Any]) -> Dict[str, Any]:
+        obligatory_params = {
+            "apikey": self.apikey,
+            "id": self.stop_resource_id,
+            **params,
+        }
+        return obligatory_params
+
+    def __get_data(self, url: str, params: Dict[str, Any]) -> Optional[Dict]:
+        response = requests.get(url, params=params)
         if response.status_code != 200:
             return None
 
@@ -38,7 +45,13 @@ class BusCaller(ICaller):
         return result.get("result", None)
 
     def get_location(self, params: LocationRequest) -> Optional[Dict]:
-        return self.__get_data(self.location_url, params)
+        params_dict = self.__get_location_obligatory_params(params.dict())
+        return self.__get_data(self.location_url, params_dict)
 
     def get_all_locations(self) -> Optional[Dict]:
-        return self.__get_data(self.location_url, LocationRequest())
+        params = self.__get_location_obligatory_params({})
+        return self.__get_data(self.location_url, params)
+
+    def get_all_stops(self) -> Optional[Dict]:
+        params = self.__get_stop_obligatory_params({})
+        return self.__get_data(self.stop_url, params=params)
